b73ea6c2d674a300b30054e4da8af4af17cce831
Halik <michaelodumosu57@gmail.com>
2019-01-06 20:01:23 -0500

    learned about how to retrieve body data in object format from a xhr request

---
+++ route_handler_handler/get_req_message.js
@@ -0,0 +1,101 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+const file_name = __filename.split("/")[__filename.split("/").length-1].split(".js")[0]
+const path = require('path')
+const fs = require('fs');
+var bodyParser = require('body-parser');
+var multer = require('multer'); // v1.0.5
+var upload = multer(); // for parsing multipart/form-data
+
+
+const getCircularReplacer = () => {
+  const seen = new WeakSet();
+  return (key, value) => {
+    if (typeof value === "object" && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+};
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.route('/book')
+  .get(function (req, res) {
+    res.send('Get a random book')
+  })
+  .post(function (req, res) {
+    res.send('Add a book')
+  })
+  .put(function (req, res) {
+      res.send('Update the book')
+
+
+    console.log(req.body)
+    const data = JSON.stringify(req.body, getCircularReplacer());    
+    const needed_file = __dirname + '/message.txt'
+
+    fs.open(needed_file, 'w+', (err, fd) => {
+        if (err){
+            throw err;
+        } 
+
+        else{
+          console.log("file opened")
+        }       
+        try{
+        let buf = Buffer.from(data)
+        pos = 0,offset = 0,
+        len = buf.length;            
+            fs.write(fd, buf, offset, len, pos,
+                (err,bytes,buff) => {
+
+
+            });     
+        }   
+        catch (err){  
+            fs.close(fd, (err) => {
+                if (err){
+                    throw err;
+                } 
+
+                else{
+                  console.log("file closed")
+                } 
+
+
+            });   
+        }           
+
+        fs.close(fd, (err) => {
+            if (err){
+                throw err;
+            } 
+
+            else{
+              console.log("file closed")
+            } 
+
+
+        });
+
+
+    });
+  
+
+  // const des = fs.createWriteStream(needed_file)
+  // des.write( fd,  req   ) 
+  // des.close(fd)
+
+  })  
+
+
+
+
+
+app.listen(port, () => console.log(`${file_name} app listening on port ${port}!`))
