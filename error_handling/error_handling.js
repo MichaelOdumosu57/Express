25bb136f7e3db2e508626cb4066d141173b9b2b4
Halik <michaelodumosu57@gmail.com>
2019-01-06 23:53:54 -0500

    learned about error handling

---
+++ error_handling/error_handling.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const file_name = __filename.split("/")[__filename.split("/").length-1].split(".js")[0]
+const path = require('path')
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const multer = require('multer'); // v1.0.5
+const upload = multer(); // for parsing multipart/form-data
+
+
+
+app.use("/error",function ( req, res, next) {
+	  try{
+	  		console.error(err.stack)
+	  		res.send("oh no")
+	  }
+	  catch(   err   ){
+	  		res.status(500).send('Something broke!')
+	  }
+})
+
+app.get("/error",function (req, res, next) {	
+	res.send("success")  	  
+})		
+
+
+
+
+
+app.listen(port, () => console.log(`${file_name} app listening on port ${port}!`))
